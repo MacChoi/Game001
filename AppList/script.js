b0a87a535c6f3fedc6179d6cc3eadc190d4b7969
chocho <diskbox1@daum.net>
2020-08-14 13:58:39 +0900

    commit

--- AppList/script.js
+++ AppList/script.js
@@ -1,28 +1,36 @@
 const list =[
-  {title:"최신 업데이트",img:"URL(http://2.bp.blogspot.com/--5yqttOEAsw/Urr9mw2ptsI/AAAAAAAADME/uR7L1d-5y50/s1600/howways-logo2-94x94.png)",url:"http://daum.net"},
-  // {title:"게임1",img:"",url:""},
-  // {title:"게임2",img:"",url:""},
-  // {title:"게임3",img:"",url:""},
-  // {title:"게임4",img:"",url:""},
+  {title:"최신 업데이트",img:"http://2.bp.blogspot.com/--5yqttOEAsw/Urr9mw2ptsI/AAAAAAAADME/uR7L1d-5y50/s1600/howways-logo2-94x94.png",url:"http://daum.net"},
+  {title:"최신 업데이트",img:"http://2.bp.blogspot.com/--5yqttOEAsw/Urr9mw2ptsI/AAAAAAAADME/uR7L1d-5y50/s1600/howways-logo2-94x94.png",url:"http://daum.net"},
+  {title:"최신 업데이트",img:"http://2.bp.blogspot.com/--5yqttOEAsw/Urr9mw2ptsI/AAAAAAAADME/uR7L1d-5y50/s1600/howways-logo2-94x94.png",url:"http://daum.net"},
+  {title:"최신 업데이트",img:"http://2.bp.blogspot.com/--5yqttOEAsw/Urr9mw2ptsI/AAAAAAAADME/uR7L1d-5y50/s1600/howways-logo2-94x94.png",url:"http://daum.net"},
+  {title:"최신 업데이트",img:"http://2.bp.blogspot.com/--5yqttOEAsw/Urr9mw2ptsI/AAAAAAAADME/uR7L1d-5y50/s1600/howways-logo2-94x94.png",url:"http://daum.net"},
+  {title:"최신 업데이트",img:"http://2.bp.blogspot.com/--5yqttOEAsw/Urr9mw2ptsI/AAAAAAAADME/uR7L1d-5y50/s1600/howways-logo2-94x94.png",url:"http://daum.net"},
+  {title:"최신 업데이트",img:"http://2.bp.blogspot.com/--5yqttOEAsw/Urr9mw2ptsI/AAAAAAAADME/uR7L1d-5y50/s1600/howways-logo2-94x94.png",url:"http://daum.net"},
+  {title:"최신 업데이트",img:"http://2.bp.blogspot.com/--5yqttOEAsw/Urr9mw2ptsI/AAAAAAAADME/uR7L1d-5y50/s1600/howways-logo2-94x94.png",url:"http://daum.net"},
+  {title:"최신 업데이트",img:"http://2.bp.blogspot.com/--5yqttOEAsw/Urr9mw2ptsI/AAAAAAAADME/uR7L1d-5y50/s1600/howways-logo2-94x94.png",url:"http://daum.net"},
+  {title:"최신 업데이트",img:"http://2.bp.blogspot.com/--5yqttOEAsw/Urr9mw2ptsI/AAAAAAAADME/uR7L1d-5y50/s1600/howways-logo2-94x94.png",url:"http://daum.net"},
+ 
 ]
 
+const img_loding = "URL(loading.gif)";
 function main(){
   var top = $("#myTop");
   var ul_list = $("#main");
 
   const element_top = list[0];
-  const text_top ='<div onclick=location.replace("'+ element_top.url +'"); class="top_title hc" style="background-size: cover;'+
-  'background-image:'+element_top.img + ';background-repeat: no-repeat;">' +
+  const text_top ='<div onclick=location.replace("'+ element_top.url +'"); class="lazyload top_title hc" style="background-size:cover;'+
+  'background-image:'+img_loding+'; background-repeat:no-repeat;" data-src='+list[0].img+ ';>' +
   '<a href="#">'+ element_top.title +'</a></div>';
   top.append(text_top);
   
   for (let index = 1; index < list.length; index++) {
     const element = list[index];
-    const text_add = '<li><div onclick=location.replace("'+ element.url +'"); class="hero" style="background-size: cover;'+
-    'background-image:'+element.img + ';background-repeat: no-repeat;">' +
+    const text_add = '<li><div onclick=location.replace("'+ element.url +'"); class="lazyload hero" style="background-size:cover;'+
+    'background-image:'+img_loding+'; background-repeat:no-repeat;" data-src='+list[index].img+ ';>'+ 
     '<a href="#">'+ element.title +'</a></div></li>';
     ul_list.append(text_add);
   }
+  $("div.lazyload").lazyload();
 }
 
 function myFunction() {
